c69ecd3faa3396c7b137f1781dcaa7d2a8a3fb11
Juan Marcos Cordobez <juanmarcoscordobez@gmail.com>
2021-07-18 21:26:53 -0300

    reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,14 +1,17 @@
-
 const initialState = {
-    
-};
 
-
-
-function rootReducer(state = initialState, action) {
-    
-
-    return state;
 }
+
+function rootReducer(state = initialState, { type, payload }) {
+    switch (type) {
+
+    case typeName:
+        return { ...state, ...payload }
+
+    default:
+        return state
+    }
+}
+
 
 export default rootReducer;
