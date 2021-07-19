e7a6a7c00ec439836b76fe3fb51b40e29d305720
Juan Marcos Cordobez <juanmarcoscordobez@gmail.com>
2021-07-18 22:59:20 -0300

    reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,15 +1,38 @@
-const initialState = {
+export const ALL_COUTRYS = 'ALL_COUTRYS';
+export const NAME_COUTRYS = 'NAME_COUTRYS';
+export const ORDEN = 'ORDEN';
+export const FILTER = 'FILTER';
+export const PAGE = 'PAGE';
+export const DETALLE_COUNTRY = 'DETALLE_COUNTRY';
 
+const initialState = {
+    allCountrys: []
 }
 
 function rootReducer(state = initialState, { type, payload }) {
     switch (type) {
 
-    case typeName:
-        return { ...state, ...payload }
+        case ALL_COUTRYS:
+            return { 
+                ...state,
+                allCountrys: payload 
+            }
+        case NAME_COUTRYS:
+            return{ 
+                ...state,
+                allCountrys: payload 
+            }
+        case FILTER:
+            return { ...state, ...payload }
+        case ORDEN:
+            return { ...state, ...payload }
+        case PAGE:
+            return { ...state, ...payload }
+        case DETALLE_COUNTRY:
+            return { ...state, ...payload }
 
-    default:
-        return state
+        default:
+            return state
     }
 }
 
