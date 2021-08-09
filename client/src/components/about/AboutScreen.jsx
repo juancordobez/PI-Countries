7201704e033b010e554eac355e9c4d59e2e461d9
Juan Marcos Cordobez <juanmarcoscordobez@gmail.com>
2021-08-09 20:46:44 -0300

    about/text

--- client/src/components/about/AboutScreen.jsx
+++ client/src/components/about/AboutScreen.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 export const AboutScreen = () => {
     return (
         <div>
-            About
+            <h1>Proyecto individual de bootcamp Henry</h1>
+            <h3>Objetivos del proyecto</h3>
+            <li>
+                <ul>Construir una App utlizando React, Redux, Node y Sequelize.</ul>
+                <ul>Afirmar y conectar los conceptos aprendidos en la carrera.</ul>
+                <ul>Aprender mejores prácticas.</ul>
+                <ul>Aprender y practicar el workflow de GIT.</ul>
+                <ul>Usar y practicar testing.</ul>
+            </li>
             
         </div>
     )
